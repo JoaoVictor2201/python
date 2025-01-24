a5215cdc026c4e3571bcf0866693ff075020a796
JoaoVictor2201 <franca.jv@hotmail.com>
2025-01-24 20:50:30 -0300

    opções do programa

--- lista-de-tarefas/lista-de-tarefas.py
+++ lista-de-tarefas/lista-de-tarefas.py
@@ -1,3 +1,14 @@
+def adicionar_tarefa(tarefas, titulo='Nao informado'):
+  tarefa = {
+    "nome": titulo,
+    "concluida": True
+  }
+  tarefas.append(tarefa)
+  print(f'Tarefa -{titulo}- adicionada')
+  return
+
+tarefas = [] # Iniciando a lista de tarefas
+
 while True:
   print('\nMenu do Gerenciador de Lista de Tarefas')
   print('1. Adicionar tarefa')
@@ -9,8 +20,20 @@ while True:
 
   escolha = int(input('Digite a sua escolha: '))
 
-  if escolha > 6: # Opão que não está no programa
-    print('Escolha uma opção válida!')
+  if escolha == 1:
+    titulo = input('Informe o nome da tarefa: ')
+    adicionar_tarefa(tarefas, titulo)
 
-  if escolha == 6: # Usuário escolheu sair do programa
+  elif escolha == 2:
+    for tarefa in tarefas:
+      print(f'[ ]' if tarefa["concluida"] == False else '[x]', end=' - ')
+      print(tarefa["nome"])
+  
+  elif escolha == 3:
     break
+
+  elif escolha == 6:
+    break
+
+  else: # Opão que não está no programa
+    print('Escolha uma opção válida!')
